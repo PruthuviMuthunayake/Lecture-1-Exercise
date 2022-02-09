e476921eb21e42dc18231a002cad24d0dd72cc88
PruthuviMuthuna <99176469+PruthuviMuthunayake@users.noreply.github.com>
2022-02-09 14:47:50 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Lecture-1-Exercise/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Janaka Pruthuvi";
+  std::cout << std::endl;
+  std::cout << "13/1 Sunandara road,";
+  std::cout << std::endl;
+  std::cout << "Kalubowila, Dehiwala.";
+  std::cout << std::endl;
+
+  return 0;
+}
